7cf2d89a45e75d3905e7d84a7585a5eb31c865aa
Bono <kubonkwan99@gmail.com>
2022-08-21 21:57:17 +0800

    Add dragscroll test and now refresh load at top
    Includes minor fixes on homepage and new content access as work3

---
+++ src/assets/js/main.js
@@ -0,0 +1,18 @@
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
+
+setTimeout(() => document.body.classList.add('render'), 60);
+// const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+const navigate = (linkEl) => {
+	document.body.classList.remove('render');
+	document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+};
+navdemos.forEach(link => link.addEventListener('click', (ev) => {
+	ev.preventDefault();
+	navigate(ev.target);
+}));
